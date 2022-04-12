172babb22a7bee21e5189c65dbe155ef9578e21b
JumaOchi <ochiengjuma95@gmail.com>
2022-04-12 14:56:51 -0700

    my commit message

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,22 @@
-#include <stdio.h>
+#include "_putchar.h"
 
 /**
-* 
+* main - Prints putchar
 *
 * Return: Always 0 (Success/correct)
 */
 
 int main(void){
 
-printf("_putchar")
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
 return 0;
 }
