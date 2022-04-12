a2b0d6c64a89e985cbf012ac9372ee054d38afd6
JumaOchi <ochiengjuma95@gmail.com>
2022-04-12 14:41:27 -0700

    My initial commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* 
+*
+* Return: Always 0 (Success/correct)
+*/
+
+int main(void){
+
+printf("_putchar")
+
+return 0;
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This folder contains attempts on tasks functions,Nested loops
