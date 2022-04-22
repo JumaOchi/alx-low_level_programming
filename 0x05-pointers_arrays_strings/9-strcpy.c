2d1b0f7157230f6422143107858b87e62d61dedd
Juma Ochi <67126769+JumaOchi@users.noreply.github.com>
2022-04-22 07:05:45 +0300

    Create 9-strcpy.c
    commit message to task no. 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * *_strcpy - return a copy of a string
+ *
+ * @dest: char
+ * @src: char
+ * Return: char
+ */
+char *_strcpy(char *dest, char *src)
+{
+	src = *dest;
+	return (src);
+}
