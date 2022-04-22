4bbc53a76f4f6d1ee1e75f729f8814030426d9fc
Juma Ochi <67126769+JumaOchi@users.noreply.github.com>
2022-04-22 07:16:43 +0300

    Update 9-strcpy.c
    new changes committed

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,13 +1,24 @@
+#include<stdio.h>
 #include "main.h"
 /**
- * *_strcpy - return a copy of a string
- *
- * @dest: char
- * @src: char
- * Return: char
+ *_strcpy - update value.
+ *@dest: value to be evaluate.
+ *@src: value to be evaluate.
+ *Return: not.
  */
 char *_strcpy(char *dest, char *src)
 {
-	src = *dest;
-	return (src);
+	int l = 0;
+	int x = 0;
+
+	while (*(src + l) != '\0')
+	{
+		l++;
+	}
+	for ( ; x < l ; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[l] = '\0';
+	return (dest);
 }
